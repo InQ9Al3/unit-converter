b80b8ef690672d2425b5a9ceda085799e0972fe7
InQ9Al3 <118564919+InQ9Al3@users.noreply.github.com>
2023-02-03 16:35:35 +0100

    commint

--- index.js
+++ index.js
@@ -3,3 +3,50 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const inputEl = document.getElementById("input-el")
+
+const converBtn = document.getElementById("convert-btn")
+
+converBtn.addEventListener("click", function() {
+    const inputValue = inputEl.value
+    console.log(inputValue)
+    console.log(
+        converToFeet(inputValue),
+        convertToMeter(inputValue),
+        convertToGallon(inputValue),
+        convertToLiter(inputValue),
+        convertToPound(inputValue),
+        convertToKg(inputValue)
+    )
+})
+
+function converToFeet(value) {
+    feetValue = value * 3.281
+    return feetValue.toFixed(3)
+}
+
+function convertToMeter(value) {
+    meterValue = value / 3.281
+    return meterValue.toFixed(3)
+}
+
+function convertToGallon(value) {
+    gallonValue = value * 0.264
+    return gallonValue.toFixed(3)
+}
+
+function convertToLiter(value) {
+    literValue = value / 0.264
+    return literValue.toFixed(3)
+}
+
+function convertToPound(value) {
+    poundValue = value * 2.204
+    return poundValue.toFixed(3)
+}
+
+function convertToKg(value) {
+    kgValue = value / 2.204
+    return kgValue.toFixed(3)
+}
