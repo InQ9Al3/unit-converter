98380b72f87754ffb87f6b5441a4d0535691a5dc
InQ9Al3 <118564919+InQ9Al3@users.noreply.github.com>
2023-02-05 20:58:01 +0100

    prettier

--- index.js
+++ index.js
@@ -12,78 +12,81 @@ const massEl = document.getElementById("mass-output")
 
 console.log("start value : " + inputEl.value)
 
-converBtn.addEventListener("click", function() {
-    run()
-    console.log("button value : " + inputEl.value)
+converBtn.addEventListener("click", function () {
+  run()
+  console.log("button value : " + inputEl.value)
 })
 
-document.addEventListener("keydown", function(event){
-    if(event.key === "Enter"){
-        
-        run()
-        console.log("enter value : " + inputEl.value)
-        // if (inputValue != null) {
-        //     run()
-        // }
-        
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    run()
+    console.log("enter value : " + inputEl.value)
+    // if (inputValue != null) {
+    //     run()
+    // }
+  }
 })
 
 function getOutput() {
-    const inputValue = inputEl.value
-    
-    lengthEl.textContent = `${inputValue} meters = ${converToFeet(inputValue)} feet | 
+  const inputValue = inputEl.value
+
+  lengthEl.textContent = `${inputValue} meters = ${converToFeet(
+    inputValue
+  )} feet | 
     ${inputValue} feet = ${convertToMeter(inputValue)} meters`
-    
-    volumeEL.textContent = `${inputValue} liters = ${convertToGallon(inputValue)} gallons | 
+
+  volumeEL.textContent = `${inputValue} liters = ${convertToGallon(
+    inputValue
+  )} gallons | 
     ${inputValue} gallons = ${convertToLiter(inputValue)} liters`
 
-    massEl.textContent = `${inputValue} kilos = ${convertToPound(inputValue)} pounds | 
+  massEl.textContent = `${inputValue} kilos = ${convertToPound(
+    inputValue
+  )} pounds | 
     ${inputValue} pounds = ${convertToKg(inputValue)} kilos`
-} 
+}
 
 function makeEmpty() {
-    lengthEl.textContent = ""
-    volumeEL.textContent = ""
-    massEl.textContent = ""
- 
+  lengthEl.textContent = ""
+  volumeEL.textContent = ""
+  massEl.textContent = ""
 }
 
 function run() {
-    const inputValue = inputEl.value
-    if (inputValue == 0){
-        makeEmpty()    
-    } else {
-        getOutput()    
-    } 
+  const inputValue = inputEl.value
+  if (inputValue == 0) {
+    makeEmpty()
+  } else {
+    getOutput()
+  }
 }
 
 function converToFeet(value) {
-    feetValue = value * 3.281
-    return feetValue.toFixed(3)
+  feetValue = value * 3.281
+  return feetValue.toFixed(3)
 }
 
 function convertToMeter(value) {
-    meterValue = value / 3.281
-    return meterValue.toFixed(3)
+  meterValue = value / 3.281
+  return meterValue.toFixed(3)
 }
 
 function convertToGallon(value) {
-    gallonValue = value * 0.264
-    return gallonValue.toFixed(3)
+  gallonValue = value * 0.264
+  return gallonValue.toFixed(3)
 }
 
 function convertToLiter(value) {
-    literValue = value / 0.264
-    return literValue.toFixed(3)
+  literValue = value / 0.264
+  return literValue.toFixed(3)
 }
 
 function convertToPound(value) {
-    poundValue = value * 2.204
-    return poundValue.toFixed(3)
+  poundValue = value * 2.204
+  return poundValue.toFixed(3)
 }
 
 function convertToKg(value) {
-    kgValue = value / 2.204
-    return kgValue.toFixed(3)
+  kgValue = value / 2.204
+  return kgValue.toFixed(3)
 }
