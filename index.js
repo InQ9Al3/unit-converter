8b7ca978633c2b7f78eee0c1572cceb24c27c2a8
InQ9Al3 <118564919+InQ9Al3@users.noreply.github.com>
2023-02-06 12:40:25 +0100

    commit

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+// const actions = require("./modules")
+// const multiply = actions.multiply
+// const divide = actions.divide
+const meterWithFeet = 3.281
+const literWithGallon = 0.264
+const kgWithPound = 2.204
 
 const inputEl = document.getElementById("input-el")
 const converBtn = document.getElementById("convert-btn")
@@ -27,20 +33,23 @@ document.addEventListener("keydown", function (event) {
 const getOutput = () => {
   const inputValue = inputEl.value
 
-  lengthEl.textContent = `${inputValue} meters = ${convertToFeet(
-    inputValue
+  lengthEl.textContent = `${inputValue} meters = ${multiply(
+    inputValue,
+    meterWithFeet
   )} feet | 
-    ${inputValue} feet = ${convertToMeter(inputValue)} meters`
+    ${inputValue} feet = ${divide(inputValue, meterWithFeet)} meters`
 
-  volumeEL.textContent = `${inputValue} liters = ${convertToGallon(
-    inputValue
+  volumeEL.textContent = `${inputValue} liters = ${multiply(
+    inputValue,
+    literWithGallon
   )} gallons | 
-    ${inputValue} gallons = ${convertToLiter(inputValue)} liters`
+    ${inputValue} gallons = ${divide(inputValue, literWithGallon)} liters`
 
-  massEl.textContent = `${inputValue} kilos = ${convertToPound(
-    inputValue
+  massEl.textContent = `${inputValue} kilos = ${multiply(
+    inputValue,
+    kgWithPound
   )} pounds | 
-    ${inputValue} pounds = ${convertToKg(inputValue)} kilos`
+    ${inputValue} pounds = ${divide(inputValue, kgWithPound)} kilos`
 }
 
 function makeEmpty() {
@@ -58,9 +67,11 @@ const run = () => {
   }
 }
 
-const convertToFeet = (value) => (value * 3.281).toFixed(3)
-const convertToMeter = (value) => (value / 3.281).toFixed(3)
-const convertToGallon = (value) => (value * 0.264).toFixed(3)
-const convertToLiter = (value) => (value / 0.264).toFixed(3)
-const convertToPound = (value) => (value * 2.204).toFixed(3)
-const convertToKg = (value) => (value / 2.204).toFixed(3)
+// const convertToFeet = (value) => (value * 3.281).toFixed(3)
+// const convertToMeter = (value) => (value / 3.281).toFixed(3)
+// const convertToGallon = (value) => (value * 0.264).toFixed(3)
+// const convertToLiter = (value) => (value / 0.264).toFixed(3)
+// const convertToPound = (value) => (value * 2.204).toFixed(3)
+// const convertToKg = (value) => (value / 2.204).toFixed(3)
+const multiply = (value, num) => (value * num).toFixed(3)
+const divide = (value, num) => (value / num).toFixed(3)
---
+++ modules.js
@@ -0,0 +1,2 @@
+exports.multiply = (value, num) => (value * num).toFixed(3)
+exports.divide = (value, num) => (value / num).toFixed(3)
